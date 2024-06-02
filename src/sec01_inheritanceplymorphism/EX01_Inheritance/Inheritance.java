1d41bcc1aa68be5418385cdf42be710688b9e392
ThisVideoLab <thisvideolab@gmail.com>
2024-06-02 23:55:45 +0900

    6월 02일자 자바 공부

---
+++ src/sec01_inheritanceplymorphism/EX01_Inheritance/Inheritance.java
@@ -0,0 +1,19 @@
+package sec01_inheritanceplymorphism.EX01_Inheritance;
+
+public class Inheritance {
+	
+	public static void main(String[]args) {
+		
+		/* 10장 클래스의 상속과 다형성
+		   
+		   10.1 클래스의 상속의 개념과 문법적 특징
+		   
+		   10.1.1 상속의 개념
+		   
+		   상속을 이해하기 위해서 간단한 예시를 들어보자. 다음과 같이
+		   
+		 */
+		
+	}
+
+}
--- src/sec02_staticmodifier/EX05_BookPractice/BookPractice.java
+++ src/sec02_staticmodifier/EX05_BookPractice/BookPractice.java
@@ -2,6 +2,32 @@ package sec02_staticmodifier.EX05_BookPractice;
 
 import sec02_staticmodifier.EX05_BookPractice.pack01.A;
 
+class Q4A{
+	int a = 2;
+	static int b = 3;
+	void abc() {
+		System.out.println(a);
+		System.out.println(b);
+	}
+	static void bcd() {
+		// System.out.println(a);
+		System.out.println(b);
+	}
+}
+
+class Q5A{
+	int a = 3;
+	static int b = 5;
+}
+
+class Q6A{
+	static int a;
+	static {
+		a = 8;
+	}
+	
+}
+
 public class BookPractice {
 
 	public static void main(String[]args) {
@@ -12,7 +38,11 @@ public class BookPractice {
 		System.out.println("public > protected > default > private");
 		System.out.println();
 		
-		/*Q2. 패키지 pack01 내에서는 A.java, B.java 파일,패키지 pack02 내에는 C.java, D.java 파일이 있다. 이들 4개의 소스 파일에서*/
+		/*Q2. 클래스 자체에 사용할 수 있는 접근 지정자를 접근 범위가 큰 순서대로 쓰시오.*/ System.out.println("Q2. 클래스 자체에 사용할 수 있는 접근 지정자를 접근 범위가 큰 순서대로 쓰시오.");
+		System.out.println("public > default");
+		System.out.println();
+		
+		/*Q3. 패키지 pack01 내에서는 A.java, B.java 파일,패키지 pack02 내에는 C.java, D.java 파일이 있다. 이들 4개의 소스 파일에서*/
 		System.out.println("Q2. 패키지 pack01 내에서는 A.java, B.java 파일,패키지 pack02 내에는 C.java, D.java 파일이 있다. 이들 4개의 소스 파일에서");
 		/*클래스 A에 포함된 4개의 필드를 사용할 때, 각 클래스 위치에서 사용할 수 있는 모든 필드를 쓰시오.*/
 		System.out.println("클래스 A에 포함된 4개의 필드를 사용할 때, 각 클래스 위치에서 사용할 수 있는 모든 필드를 쓰시오.");
@@ -84,6 +114,94 @@ public class BookPractice {
 		System.out.println("	}");
 		System.out.println("}");
 		System.out.println();
+		
+		/*4. 다음 코드는 오류를 포함하고 있다. 오류가 발생한 위치와 그 이유를 설명하시오.*/
+		System.out.println("4. 다음 코드는 오류를 포함하고 있다. 오류가 발생한 위치와 그 이유를 설명하시오.");
+		System.out.println();
+		
+		System.out.println("class Q4A{");
+		System.out.println("	int a = 2;");
+		System.out.println("	static int b = 3;");
+		System.out.println("	void abc() {");
+		System.out.println("		System.out.println(a);");
+		System.out.println("		System.out.println(b);");
+		System.out.println("	}");
+		System.out.println("	static void bcd() {");
+		System.out.println("		System.out.println(a);");
+		System.out.println("		System.out.println(b);");
+		System.out.println("	}");
+		System.out.println("}");
+		System.out.println();
+		
+		System.out.println("static void bcd() 메서드 내에 있는 변수 a에서 오류가 발생함. class Q4A가 생성되는 과정에서");
+		System.out.println("클래스 내부에 포함되어 힙영역에 저장되는 반면 인스턴스 영역은 힙 영역에 생성되기 때문에 인스턴스 내부에서");
+		System.out.println("대상을 가리키는 인스턴스 변수는 메서드가 작동함과 동시에 힙 영역에 생성됨. 그런데 힙영역에서 this 키워드를");
+		System.out.println("사용해서 static int a를 지정하려 해도 대상이 힙 영역에 없기 때문에 오류가 발생함.");
+		
+		/*5. 다음과 같이 1개의 인스턴스 필드(a)와 1개의 정적 필드(b)를 포함하고 있는 클래스 A가 정의돼 있다.*/
+		System.out.println("5. 다음과 같이 1개의 인스턴스 필드(a)와 1개의 정적 필드(b)를 포함하고 있는 클래스 A가 정의돼 있다.");
+		System.out.println();
+		
+		System.out.println("class Q5A{");
+		System.out.println("	int a = 3;");
+		System.out.println("	static int b = 5;");
+		System.out.println();
+		
+		/*이때 다음 코드의 실행 결과를 쓰시오.*/ System.out.println("이때 다음 코드의 실행 결과를 쓰시오.");
+		System.out.println();
+		
+		System.out.println("Q5A q5a1 = new Q5A();");
+		System.out.println("Q5A q5a2 = new Q5A();");
+		System.out.println();
+		System.out.println("q5a1.a = 30;");
+		System.out.println("");
+		System.out.println("q5a1.b = 40;");
+		System.out.println();
+		System.out.println("q5a2.a = 50;");
+		System.out.println("q5a2.b = 60;");
+		System.out.println();
+		
+		Q5A q5a1 = new Q5A();
+		Q5A q5a2 = new Q5A();
+		
+		System.out.println("출력 결과:");		
+		System.out.println();		
+		q5a1.a = 30;
+		q5a1.b = 40;
+		
+		q5a2.a = 50;
+		q5a2.b = 60;
+		
+		System.out.println(q5a1.a);
+		System.out.println(q5a1.b);
+		System.out.println();
+		
+		System.out.println(q5a2.a);
+		System.out.println(q5a2.b);
+		System.out.println();
+		
+		/*6. 클래스 A가 다음과 같이 공란으로 선언돼 있다. 이 때 출력부분의 결과값이 8이 나오토록 클래스 A를 완성하시오.*/
+		System.out.println("6. 클래스 Q6A가 다음과 같이 공란으로 선언돼 있다. 이 때 출력부분의 결과값이 8이 나오토록 클래스 Q6A를 완성하시오.");
+		System.out.println();
+		
+		System.out.println("class Q6A{");
+		System.out.println();
+		System.out.println("	// 공란");
+		System.out.println();
+		System.out.println("}");
+		System.out.println();
+		
+		System.out.println("출력부: ");
+		System.out.println("System.out.println(Q6A.a);");
+		System.out.println();
+		
+		System.out.println("공란에 들어갈 내용: ");
+		System.out.println("	static {");
+		System.out.println("		a = 8;");
+		System.out.println("	}");
+		System.out.println();
+			
+		System.out.println(Q6A.a);
 		
 	}
 	
